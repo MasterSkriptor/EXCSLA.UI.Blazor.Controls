a8e386104a2a18c7bf991bc2caf1ba9183d9335d
Harold E Collins <masterskriptor@hotmail.com>
2021-11-04 10:43:54 -0500

    Added ReportBar into project

---
+++ src/EXCSLA.UI.Blazor.Controls/ReportBar/ReportBar.razor.cs
@@ -0,0 +1,25 @@
+﻿using EXCSLA.UI.Blazor.Services;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EXCSLA.UI.Blazor.Controls
+{
+    public partial class ReportBar : ComponentBase
+    {
+        [Inject] public IColorPicker ColorPicker { get; set; }
+
+        [Parameter] public Dictionary<string, int> Items { get; set; }
+        [Parameter] public string SelectedItem { get; set; }
+        [Parameter] public EventCallback<string> OnSelectedItemChanged { get; set; }
+
+        protected async Task HandleOnSelectedItemChanged(string value)
+        {
+            SelectedItem = Items != null && Items.Count > 0 ? Items.Where(i => i.Key == value).FirstOrDefault().Key : string.Empty;
+            StateHasChanged();
+            
+            await OnSelectedItemChanged.InvokeAsync(SelectedItem);
+        }
+    }
+}
