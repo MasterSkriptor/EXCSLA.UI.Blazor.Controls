d77f9f027c31fd2e828fc0119c17d7df6abb5c95
Harold E Collins <masterskriptor@hotmail.com>
2022-01-08 11:34:14 -0600

    Added new Control TemplatedDataViewer. Allows for viewing data without the form validation.

---
+++ src/EXCSLA.UI.Blazor.Controls/TemplatedDataViewer/TemplatedDataViewer.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EXCSLA.UI.Blazor.Controls
+{
+    public partial class TemplatedDataViewer<TItem> : ComponentBase
+    {
+        [Parameter] public List<TItem> Items { get; set; }
+        [Parameter] public TItem SelectedItem { get; set; }
+
+        [Parameter] public RenderFragment<TItem> ItemTemplate { get; set; }
+        [Parameter] public RenderFragment<TItem> ViewerTemplate { get; set; }
+        [Parameter] public RenderFragment NavBarTemplate { get; set; }
+
+        [Parameter] public EventCallback<string> OnSearch { get; set; }
+        [Parameter] public EventCallback<TItem> OnItemSelected { get; set; }
+        [Parameter] public bool ListIsSearchable { get; set; } = false;
+
+        [Parameter] public string LoadingMessage { get; set; } = "Loading...";
+        [Parameter] public string SearchText { get; set; } = string.Empty;
+        [Parameter] public string LoadingAnimationColor { get; set; } = "#206994";
+        [Parameter] public string LogoUrl { get; set; } = "../imgs/logo.png";
+        [Parameter] public Dictionary<string, List<string>> Errors { get; set; }
+
+        protected async Task HandleSearch(string searchString)
+        {
+            SearchText = searchString;
+            await OnSearch.InvokeAsync(searchString);
+        }
+
+        protected async Task HandleItemSelected(TItem item)
+        {
+            SelectedItem = item;
+            await OnItemSelected.InvokeAsync(item);
+        }
+    }
+}
